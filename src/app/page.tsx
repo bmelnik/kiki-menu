80175e4ab9709e8dbab44fe662727beeaa9fd197
Boris Melnik <bmelnik0@gmail.com>
2026-01-14 14:31:22 +0200

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 // Menu data
 const tapBeer = [
